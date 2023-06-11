ddb142f99251f7080bf70b903eb4f6ba1e1cc5d0
gongmingye <gliberte@163.com>
2023-06-11 23:31:46 +0800

    first commit

---
+++ src/main/java/com/gmy/Main.java
@@ -0,0 +1,11 @@
+package com.gmy;
+
+/**
+ * @author gmy
+ * @date 2023/6/11 23:30
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
