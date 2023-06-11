3fe3b83164d01cafb5ce958436773c918f483a37
gongmingye <gliberte@163.com>
2023-06-12 00:06:50 +0800

    one 1

---
+++ src/main/java/com/gmy/One.java
@@ -0,0 +1,11 @@
+package com.gmy;
+
+/**
+ * @author gmy
+ * @date 2023/6/12 0:05
+ */
+public class One {
+    public static void main(String[] args) {
+        System.out.println("hello one");
+    }
+}
