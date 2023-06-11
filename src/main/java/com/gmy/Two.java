8faad66b6fa5006e6423f2e5b57e428747ed4d39
gongmingye <gliberte@163.com>
2023-06-12 00:16:16 +0800

    two 1

---
+++ src/main/java/com/gmy/Two.java
@@ -0,0 +1,11 @@
+package com.gmy;
+
+/**
+ * @author gmy
+ * @date 2023/6/12 0:15
+ */
+public class Two {
+    public static void main(String[] args) {
+        System.out.println("two");
+    }
+}
