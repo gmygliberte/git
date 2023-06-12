17eea279fd4002578eea87fd541d7c29fb9f3fd5
gongmingye <gliberte@163.com>
2023-06-12 15:41:29 +0800

    two 3

--- src/main/java/com/gmy/Two.java
+++ src/main/java/com/gmy/Two.java
@@ -8,5 +8,6 @@ public class Two {
     public static void main(String[] args) {
         System.out.println("two 1");
         System.out.println("two 2");
+        System.out.println("two 3");
     }
 }
