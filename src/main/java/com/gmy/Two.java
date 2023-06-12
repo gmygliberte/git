f33995af4ed3b2a5310664a3b4b16328617c95e7
gongmingye <gliberte@163.com>
2023-06-12 15:40:56 +0800

    cherry two2

---
+++ src/main/java/com/gmy/Two.java
@@ -0,0 +1,12 @@
+package com.gmy;
+
+/**
+ * @author gmy
+ * @date 2023/6/12 15:40
+ */
+public class Two {
+    public static void main(String[] args) {
+        System.out.println("two 1");
+        System.out.println("two 2");
+    }
+}
