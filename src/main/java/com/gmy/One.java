ce189d1cabdbba583169cc44872d9c9449b194c6
gongmingye <gliberte@163.com>
2023-06-12 15:24:07 +0800

    sum commit

--- src/main/java/com/gmy/One.java
+++ src/main/java/com/gmy/One.java
@@ -8,5 +8,9 @@ public class One {
     public static void main(String[] args) {
         System.out.println("hello one");
         System.out.println("hello one");
+        System.out.println("尝试直接merge到main分支上");
+        System.out.println("本地one分支提交到two分支");
+        System.out.println("one 4");
+        System.out.println("one 5");
     }
 }
